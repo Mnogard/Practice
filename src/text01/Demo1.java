932055b3394943217d123ae5ae6e55823143dd86
Momo <1157738285@qq.com>
2021-02-17 17:34:17 +0800

    Hello!Fan!Zai!

--- src/text01/Demo1.java
+++ src/text01/Demo1.java
@@ -2,6 +2,6 @@ package text01;
 
 public class Demo1 {
     public static void main(String[] args) {
-        System.out.println("Hello!Fan!");
+        System.out.println("Hello!Fan!Zai!");
     }
 }
